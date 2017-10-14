fe7b47a3771ab98ad3a661c9471817abb880702c
Clara <cloppenheimer@gmail.com>
2017-10-13 23:49:13 -0400

    updating setup

--- setup.js
+++ setup.js
@@ -1,1 +1,29 @@
+var express = require('express');
 
+var bodyParser = require('body-parser'); // Required if we need to use HTTP query or post parameters
+var validator = require('validator'); 
+
+var mongoUri = process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/polyhack2017';
+var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
+        db = databaseConnection;
+});
+
+function loadData(interests){
+        var name = document.getElementById("name").innerHTML;
+        var username = document.getElementById("username").innerHTML;
+        var age = document.getElementById("age").innerHTML;
+        var gender = document.getElementById("gender").innerHTML;
+
+        var toInsert = {
+                "name": name,
+                "username": username,
+                "age": age,
+                "gender": gender,
+                "interests": interests
+        };
+
+        db.collection("userInfo", function(error, coll) {
+                db.coll.insert(toInsert);
+        });
+}
